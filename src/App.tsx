a434928705cc01b12a9278f86b525286947d4017
Loa212 <anobileloris@gmail.com>
2025-01-07 17:37:44 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="content bg-black text-white p-4">
+      <h1 className="text-4xl">Rsbuild with React</h1>
+      <p>Start building amazing things with Rsbuild.</p>
+    </div>
+  );
+};
+
+export default App;
